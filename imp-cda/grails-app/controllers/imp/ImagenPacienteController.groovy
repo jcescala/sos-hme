2789c8bfa3dc89031a22551e82c1cb0b8fcc1c77
armandodj5 <armandodj5@gmail.com>
2012-03-08 21:22:51 +0000

    imagen para pacientes en imp sin imagen

--- imp-cda/grails-app/controllers/imp/ImagenPacienteController.groovy
+++ imp-cda/grails-app/controllers/imp/ImagenPacienteController.groovy
@@ -17,14 +17,27 @@ class ImagenPacienteController {
         if(impService.existePaciente(params.idPaciente, org.uniqueIdentifier)){
             paciente = Paciente.findByIdPacienteOrgAndCentro(params.idPaciente, org)
             if(!paciente.imagen || !paciente.tipoImagen){
-                response.sendError(404)
-                return
-            }
+                
+                //El paciente no tiene imagen
+                //response.sendError(404)
+            File f = new File('web-app/images/pacientes/silueta.gif')
+            
+            
+            response.setContentType('image/gif')
+            response.setContentLength((int)f.size())
+            OutputStream out = response.getOutputStream()
+            out.write(f.getBytes())
+            out.close()    
+                
+                
+            
+            }else{
             response.setContentType(paciente.tipoImagen)
             response.setContentLength(paciente.imagen.size())
             OutputStream out = response.getOutputStream()
             out.write(paciente.imagen)
             out.close()
+            }
         }else{
             response.sendError(404)
             return
